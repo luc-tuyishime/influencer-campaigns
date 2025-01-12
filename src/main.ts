195e6b4ba75a632ce26ffca7a2e35d3d7f8711b7
luc-tuyishime <luctunechi45@gmail.com>
2025-01-12 12:55:50 +0200

    configure railway

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,12 +21,11 @@ async function bootstrap() {
     }),
   );
 
+
   app.setGlobalPrefix('api');
 
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('port');
-
+  const port = process.env.PORT || 3001;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
